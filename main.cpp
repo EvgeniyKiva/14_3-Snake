7945e0f6bff0df74144e8b6bacefab96983da857
EvgeniyKiva <97800267+EvgeniyKiva@users.noreply.github.com>
2022-02-21 19:13:51 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 /*
 0 1 2 3 4
-
 9 8 7 6 5
-
 10 11 12 13 14
-
 19 18 17 16 15
-
 20 21 22 23 24
  */
 int main() {
@@ -15,21 +11,22 @@ int main() {
     int element=0;
     int j=0;
 
-    for (int i=0; i<5; i+=2){
+    for (int i=0; i<5; i++){
         for (j=0; j<5; j++){
-            matrix[i][j] = element;
-            element++;
+            if (i%2==0){
+                matrix[i][j] = element;
+                element++;
+            }
+
+            else{
+                matrix[i][4-j]=element;
+                element++;
+            }
+
         }
-        element+=5;
     }
-    element=5;
-    for(int i=1; i<5 ; i+=2){
-        for (int j=4; j>=0; j--){
-            matrix[i][j] = element;
-            element++;
-        }
-        element+=5;
-    }
+
+
     for (int i=0; i<5; i++){
         for (int j=0; j<5; j++){
             std::cout << matrix[i][j] << " " ;
